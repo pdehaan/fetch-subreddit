e96d1e03d0b6cb7bade33b90659cb5dfe44c2d06
Peter deHaan <peter@deseloper.com>
2016-08-15 12:51:29 -0700

    Allow for multiple subreddits via the CLI

--- bin/fetch-subreddit.js
+++ bin/fetch-subreddit.js
@@ -1,20 +1,27 @@
 #!/usr/bin/env node
 
 const yargs = require('yargs');
-const {fetchMetadata, fetchSubreddit} = require('../index');
+const {fetchSubreddit} = require('../index');
 
-const argv = yargs.usage('$0 {subreddit}')
+const argv = yargs.usage('$0 {subreddit} [{subreddit}]')
   .demand(1, 'must provide a valid command')
   .help('h')
   .alias('h', 'help')
   .argv;
 
-const subreddit = argv._[0];
+const subreddits = argv._.map(fetchSubreddit);
 
-fetchSubreddit(subreddit)
-  .then(({urls}) => fetchMetadata(urls))
-  .then((urls) => console.log(pretty(urls)))
+Promise.all(subreddits)
+  .then(flatten)
+  .then((data) => console.log(pretty(data)))
   .catch((err) => console.error(err));
+
+function flatten(data) {
+  return data.reduce((prev, curr) => {
+    Object.keys(curr).forEach((key) => prev[key] = curr[key]);
+    return prev;
+  }, {});
+}
 
 function pretty(obj) {
   return JSON.stringify(obj, null, 2);
