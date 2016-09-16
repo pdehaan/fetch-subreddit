4b06d8b1c98da4937c9c90f60569ce09994fd584
Peter deHaan <peter@deseloper.com>
2016-09-16 12:45:45 -0700

    Minor cleanup before I refactor and add a few more methods (#5)

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
 const fetch = require('node-fetch');
-const sortjson = require('sort-json');
 
 module.exports = {
   fetchSubreddit,
   pretty
 };
 
+/**
+ * Get a subreddit's links by subreddit name (or array of subreddit names).
+ *
+ * @param  {String|Array} subreddits Subreddit name or array of subreddit names.
+ * @return {Object} An Object containing subreddit names and nested submission links.
+ */
+function fetchSubreddit(subreddits) {
+  if (!Array.isArray(subreddits)) {
+    subreddits = [subreddits];
+  }
+
+  subreddits = subreddits.map(_fetchSubreddit);
+  return Promise.all(subreddits)
+    .then(_flatten);
+}
+
+/**
+ * Internal method to fetch the JSON feed and extract the URLs.
+ * @private
+ * @param  {String} subreddit Subreddit name to fetch JSON feed for.
+ * @return {Object} An object containing the subreddit name as a key, and links as a nested array.
+ */
 function _fetchSubreddit(subreddit) {
   const reducer = (prev, {data: {url}}) => prev.push(url) && prev;
   const jsonUri = `https://www.reddit.com/r/${subreddit}/.json`;
@@ -16,21 +37,22 @@ function _fetchSubreddit(subreddit) {
     .then((urls) => ({[subreddit]: urls}));
 }
 
-function fetchSubreddit(subreddits) {
-  if (!Array.isArray(subreddits)) {
-    subreddits = [subreddits];
-  }
-
-  subreddits = subreddits.map(_fetchSubreddit);
-  return Promise.all(subreddits)
-    .then(flatten);
-}
-
+/**
+ * Convenience wrapper for `JSON.stringify()`.
+ * @param  {Object} obj Object to stringify.
+ * @return {String}     Stringified object.
+ */
 function pretty(obj) {
   return JSON.stringify(obj, null, 2);
 }
 
-function flatten(data) {
+/**
+ * Internal method to merge two objects.
+ * @private
+ * @param  {Array} data Array to reduce.
+ * @return {Object} An Object containing the merged array elements.
+ */
+function _flatten(data) {
   return data.reduce((prev, curr) => {
     for (let key in curr) {
       prev[key] = curr[key];
