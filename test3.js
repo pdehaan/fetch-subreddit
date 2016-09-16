0be9a23af21be25849d0e87acfef8274bd892deb
Peter deHaan <peter@deseloper.com>
2016-09-16 15:12:36 -0700

    Tweak test3 to be a bit more clearer

--- test3.js
+++ test3.js
@@ -4,54 +4,65 @@ const { fetchSubreddit, fetchRandomSubredditName, pretty } = require('./index');
 
 // Fetch 2 random subreddits via /r/random, then grab each subreddits's JSON feed.
 fetchRandomSubredditName(2)
-  .then((subreddits) => fetchSubreddit(subreddits.map(({name}) => name)))
+  // Pluck out just the `name` property from each subreddit.
+  .then((subreddits) => subreddits.map(({name}) => name))
+  // Fetch each subreddit's JSON feed.
+  .then((subreddits) => fetchSubreddit(subreddits))
+  // Run a reducer on the fetched URLs and group by hostname.
   .then((subreddits) => subreddits.map(({subreddit, urls}) => {
-    // Run a reducer on the fetched URLs and group by hostname.
     return {
       subreddit,
-      urls: urls.reduce((prev, curr) => {
-        const hostname = urlParse(curr).hostname;
-        prev[hostname] = prev[hostname] || 0;
-        prev[hostname] += 1;
-        return prev;
-      }, {})
+      urls: urls.reduce(_subredditReducer, {})
     };
   }))
+  // Optional: Reduce all subreddits into a single object.
+  .then((subreddits) => subreddits.reduce(_summaryReducer, {}))
   .then((subreddits) => pretty(subreddits))
   .then((output) => console.log(output))
   .catch((err) => console.error(err));
+
+function _subredditReducer(prev, curr) {
+  const hostname = urlParse(curr).hostname;
+  prev[hostname] = prev[hostname] || 0;
+  prev[hostname] += 1;
+  return prev;
+}
+
+function _summaryReducer(prev, {urls}) {
+  Object.keys(urls).forEach((hostname) => {
+    prev[hostname] = prev[hostname] || 0;
+    prev[hostname] += urls[hostname];
+  });
+  return prev;
+}
 
 /** OUTPUT:
 
   [
     {
-      "subreddit": "pokemonshowdown",
+      "subreddit": "Disney_Infinity",
       "urls": {
-        "www.reddit.com": 5,
-        "replay.pokemonshowdown.com": 21
+        "www.reddit.com": 17,
+        "imgur.com": 9,
+        "www.ign.com": 1
       }
     },
     {
-      "subreddit": "beer",
+      "subreddit": "AlienBlue",
       "urls": {
-        "goodbeerhunting.com": 2,
-        "www.liveleak.com": 1,
-        "www.westword.com": 1,
-        "www.pastemagazine.com": 1,
-        "www.youtube.com": 2,
-        "thebolditalic.com": 1,
-        "tenemu.com": 1,
-        "www.reddit.com": 8,
-        "www.telegram.com": 1,
-        "www.nuvo.net": 1,
-        "twitter.com": 1,
-        "brewedculture.org": 1,
-        "craftconscious.co": 1,
-        "www.beernloathing.com": 1,
-        "sfist.com": 1,
-        "www.analogagogo.com": 1
+        "www.reddit.com": 25,
+        "i.reddituploads.com": 1
       }
     }
   ]
+
+Or, if you run the optional extra reducer above, all the subreddits are merged into a single object with combined counts:
+
+  {
+    "www.reddit.com": 42,
+    "imgur.com": 9,
+    "www.ign.com": 1,
+    "i.reddituploads.com": 1
+  }
 
  */
